1f589e795809ed075ababd9ef52466cc9596d790
Leon Schmidt <leon18058r@gmail.com>
2023-08-12 11:20:13 +0200

    db update

--- shared/classes/Datenbank.php
+++ shared/classes/Datenbank.php
@@ -3,15 +3,17 @@ class DB {
     private $conn;
 
     public function __construct() {
-        $this->loadEnv();
-        $this->connect();
+        $this->loadEnv(); // Lädt Umgebungsvariablen aus .env-Datei
+        $this->connect(); // Stellt Verbindung zur Datenbank her
     }
 
+    // Lädt Umgebungsvariablen aus .env-Datei
     private function loadEnv() {
         $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ ."/../config");
         $dotenv->load();
     }
 
+    // Stellt Verbindung zur Datenbank her
     private function connect() {
         $host = $_ENV['DB_HOST'];
         $user = $_ENV['DB_USER'];
@@ -22,16 +24,21 @@ class DB {
             mysqli_report(MYSQLI_REPORT_OFF);
         }
 
-        $this->conn = new mysqli($host, $user, $pass, $dbname);
+        $this->conn = @new mysqli($host, $user, $pass, $dbname);
 
         if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
+            die("Connection failed!");
         }
     }
 
-    public function sql2array($query) {
+    // Führt SQL-Abfrage aus und gibt Ergebnis als Array zurück
+    public function sql2array(string $query) {
         $sqlArray = array();
         $result = $this->conn->query($query);
+
+        if (!$result) {
+            die("SQL Error: " . $this->conn->error);
+        }
 
         while ($row = $result->fetch_assoc()) {
             $sqlArray[] = $row;
@@ -41,12 +48,19 @@ class DB {
         return $sqlArray;
     }
 
-    public function sql2db($query) {
+    // Führt SQL-Abfrage aus und gibt das Ergebnisobjekt zurück
+    public function sql2db(string $query) {
         $result = $this->conn->query($query);
+
+        if (!$result) {
+            die("SQL Error: " . $this->conn->error);
+        }
+
         $this->close();
         return $result;
     }
 
+    // Schließt die Datenbankverbindung
     public function close() {
         $this->conn->close();
     }
@@ -54,6 +68,7 @@ class DB {
 
 // Beispielverwendung
 $db = new DB();
-$resultArray = $db->sql2array("SELECT * FROM cml_code");
+// $resultArray = $db->sql2array("SELECT * FROM cml_code");
+// $resultArray = $db->sql2db("INSERT into  cml_code (code, code_share) VALUES ('test', 'test')");
 var_dump($resultArray);
 ?>
