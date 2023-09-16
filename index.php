b955ef9c969dfc2db98316b5817777168b9410e1
Leon Schmidt <leon18058r@gmail.com>
2023-09-16 16:30:01 +0200

    index cleared

--- index.php
+++ index.php
@@ -1,64 +1,28 @@
 <?php 
 require_once 'shared/vendor/autoload.php'; 
+
 use Classes\{
     Router,
     DB,
 };
 
 $db = new DB();
-$router = new Router(basename(__DIR__));
+$router = new Router();
 
-// $router->setErrorRedirect("/");
-$router->setProjectName("Backend Version v1.1");
+//Project settings
+$router->setErrorRedirect("/");
+$router->setProjectName("MyPHPProject");
 $router->setFavicon("/favicon.ico");
-$router->disableComments();
+$router->addMeta('name="theme-color" content="black"');
+
+//Styles
+$router->addStyle("shared/web/css/styles.css");
+
+//Scripts
+$router->addScript("shared/web/js/styles.js");
 
 $router->addRoute('*', '/', function () use ($router) {
-    $router->setTitle("Home");
-    $router->addStyle('/styles.css');
-    $router->addScript('/scripts.js');
     $router->build();
-
-    echo "<h1>Your on the index of this App</h1>";
-})
-->setAlias('/pe')
-->setAlias('/pen');
-
-$router->addRoute('*', '/api', function () use ($router, $db) {
-    $router->isApi();
-
-    $test = $db->sql2array_file("test.sql", [53, 55]);
-    echo json_encode($test, JSON_PRETTY_PRINT);
-
-});
-
-$router->addRoute('POST', '/user/data', function () {
-    //Get user data only via Ajax
-})->onlyAjax();
-
-$router->addRoute('GET', '/test', function () use ($router) {
-    $router->build();
-
-    $variables = ['page' => "/test", 'url' => $router->projectName];
-    $router->getSite('test', $variables);
-});
-
-$router->addRoute('GET', '/test/:id', function () use ($router) {
-
-    $router->useController("TestController", "getTest", ["controllerName" => "TestController"]);
-
-    echo "test-id: " . $router->getRouteParam('test');
-});
-
-$router->addGroup('/admin', function($router, $prefix) {
-    $router->addRoute('GET', $prefix . '/dashboard', function () {
-        echo "Admin Dashboard";
-    });
-    $router->addRoute('GET', $prefix . '/users', function () {
-        echo "Admin Users";
-    });
-})->addMiddleware(function () {
-    echo "ADMIN MIDDLeWARE ";
 });
 
 ?>
