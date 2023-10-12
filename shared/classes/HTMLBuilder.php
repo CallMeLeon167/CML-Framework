a171f0d7f9456dd57ed60248d665a34b6b69dfb2
Leon <leon18058r@gmail.com>
2023-10-12 08:46:08 +0200

    better code doc HTMLBuilder

--- shared/classes/HTMLBuilder.php
+++ shared/classes/HTMLBuilder.php
@@ -1,7 +1,12 @@
 <?php 
 namespace Classes;
-class HTMLBuilder{
+
+/**
+ * The HTMLBuilder class is used to build HTML documents with customizable elements.
+ */
+class HTMLBuilder {
     use Traits\Traits;
+    
     private string $projectName = "";
     private string $title = "";
     private string $favicon = "";
@@ -13,47 +18,93 @@ class HTMLBuilder{
     private array $metas = [];
     private bool $showComments = true;
 
+    /**
+     * Sets the project name and updates the title accordingly.
+     *
+     * @param string $projectName The project name.
+     */
     public function setProjectName($projectName) {
         $this->projectName = $projectName;
         $this->setTitle($this->projectName);
     }
 
+    /**
+     * Sets the title of the HTML document.
+     *
+     * @param string $title The title of the HTML document.
+     */
     public function setTitle($title) {
         $this->title = $title;
     }
-    
+
+    /**
+     * Sets the author(s) of the HTML document.
+     *
+     * @param string ...$author The author(s) of the HTML document.
+     */
     public function setAuthor(...$author) {
         $this->author = implode(", ", $author);
     }
 
+    /**
+     * Sets the path to the favicon.
+     *
+     * @param string $favicon The path to the favicon.
+     */
     public function setFavicon($favicon) {
         $this->favicon = $favicon;
     }
 
+    /**
+     * Adds a header element to the HTML document.
+     *
+     * @param string $header The header element to add.
+     */
     public function addHeader($header) {
         $this->header[] = $header;
     }
 
+    /**
+     * Adds a stylesheet link to the HTML document.
+     *
+     * @param string $href The path to the stylesheet.
+     */
     public function addStyle($href) {
         $this->styles[] = $href;
     }
 
+    /**
+     * Adds a script link to the HTML document.
+     *
+     * @param string $src The path to the script.
+     */
     public function addScript($src) {
         $this->scripts[] = $src;
     }
 
+    /**
+     * Adds a meta tag to the HTML document.
+     *
+     * @param array $attrs The attributes of the meta tag.
+     */
     public function addMeta($attrs) {
         $this->metas[] = $attrs;
     }
 
+    /**
+     * Disables HTML comments in the document.
+     */
     public function disableComments() {
         $this->showComments = false;
     }
 
+    /**
+     * Builds the HTML document with configured elements and displays it.
+     */
     public function build() {
         $html = '<!DOCTYPE html>' . PHP_EOL;
         $html .= '<html>' . PHP_EOL;
-        if($this->showComments == true) $html .= $this->init_comment($this->projectName, $this->author) . PHP_EOL;
+        if ($this->showComments == true) $html .= $this->init_comment($this->projectName, $this->author) . PHP_EOL;
         $html .= '<head>' . PHP_EOL;
         $html .= '<meta charset="UTF-8">' . PHP_EOL;
         $html .= '<meta name="viewport" content="width=device-width, initial-scale=1.0">' . PHP_EOL;
@@ -61,31 +112,34 @@ class HTMLBuilder{
             $html .= '<meta ' . $meta . '>' . PHP_EOL;
         }
         $html .= '<title>' . $this->title . '</title>' . PHP_EOL;
-        $html .= '<link rel="icon" type="image/x-icon" href="'.$this->assetUrl($this->favicon).'">' . PHP_EOL;
-        
+        $html .= '<link rel="icon" type="image/x-icon" href="' . $this->assetUrl($this->favicon) . '">' . PHP_EOL;
+
         foreach ($this->styles as $style) {
             $html .= '<link rel="stylesheet" type="text/css" href="' . $this->assetUrl($style) . '">' . PHP_EOL;
         }
-        
+
         foreach ($this->scripts as $script) {
             $html .= '<script src="' . $this->assetUrl($script) . '"></script>' . PHP_EOL;
         }
-        
+
         $html .= '</head>' . PHP_EOL;
         $html .= '<body>' . PHP_EOL;
-        
+
         foreach ($this->header as $header) {
             $html .= $header . PHP_EOL;
         }
 
-        // Hier könntest du weiteren Inhalt hinzufügen
+        // Additional content can be added here
         
         echo $html;
     }
 
     /**
-     * @param string $comment String der Zentriert werden soll.
-     * @return string Returned einen zentrierten string.
+     * Centers a comment in the HTML document.
+     *
+     * @param string $comment The comment text to be centered.
+     * @param bool $center Indicates if the comment should be centered (default is true).
+     * @return string The centered comment.
      */
     public static function center_comment(string $comment, bool $center = true):string{
         $centeredCommend = "";
@@ -109,8 +163,10 @@ class HTMLBuilder{
     }
 
     /**
-     * @param string $titel Anwendungsname.
-     * @param string $programmer Namen der Programmierer.
+     * Initializes and displays a centered comment in the HTML document.
+     *
+     * @param string $title The application name.
+     * @param string ...$programmer The programmer's names.
      */
     public function init_comment(string $titel, string ...$programmer){
         $comment = array();
