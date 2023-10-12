58661f95871525a6020bde18a838f7fcb4ef9f63
Leon <leon18058r@gmail.com>
2023-10-12 13:46:14 +0200

    Router comment update + added executeMiddleware for before and after + removed Middleware params

--- app/classes/Router.php
+++ app/classes/Router.php
@@ -3,45 +3,73 @@ namespace Classes;
 use Classes\HTMLBuilder;
 
 /**
+ * Class Router
+ *
  * @author Leon.Schmidt
  */
 class Router extends HTMLBuilder{
     use \Classes\Traits\Traits;
 
     /**
-     * Stores routes
+     * Stores the defined routes.
+     *
+     * @var array
      */
     protected array $routes = [];
+
+    /**
+     * Stores the currently requested route.
+     *
+     * @var string
+     */
     protected string $currentRoute = '';
 
     /**
-     * Stores the current Method
+     * Stores the current HTTP request method.
+     *
+     * @var string
      */
     protected string $currentMethod = '';
 
     /**
-     * Stores middlewares
+     * Stores route-specific middleware functions.
+     *
+     * @var array
      */
     protected array $middlewares = [];
+
+    /**
+     * Stores global middleware functions.
+     *
+     * @var array
+     */
     protected array $globalMiddleware = [];
 
     /**
-     * Stores aliases
+     * Stores route aliases.
+     *
+     * @var array
      */
     protected array $aliases = [];
 
     /**
-     * Store the url to redirect
+     * Stores the URL to redirect to if a route is not defined.
+     *
+     * @var string
      */
     public string $redirectUrl = "";
 
     /**
-     * Store the bool if its an api route
+     * Indicates whether the route is an API route.
+     *
+     * @var bool
      */
     public bool $isApi = false;
 
     /**
-     * Store the params of an route
+     * Stores the parameters of the current route.
+     *
+     * @var array
      */
     protected array $currentRouteParams = [];
 
@@ -53,7 +81,7 @@ class Router extends HTMLBuilder{
         $dotenv->load();
         $errorfile = (__DIR__.'/../../errorlogfile.log');
         if ($_ENV["PRODUCTION"] === 'true') {
-            // In production environment, do not display errors
+            // In the production environment, do not display errors
             error_reporting(0);
             ini_set('display_errors', 0);
         } else {
@@ -66,14 +94,16 @@ class Router extends HTMLBuilder{
     }
 
     /**
-     * Match the routes 
+     * Match the defined routes.
      */
     public function __destruct() {
         $this->matchRoute();
     }
 
     /**
-     * Set a tht isApi to true
+     * Set the route as an API route.
+     *
+     * @return bool
      */
     public function isApi(){
         header('Content-Type: application/json');
@@ -81,8 +111,7 @@ class Router extends HTMLBuilder{
     }
 
     /**
-     * This static method is called from inside of the class,
-     * to close the application correctly at the end.
+     * Close the application correctly.
      */
     public static function APP_CLOSE() {
         echo PHP_EOL .'</body>';
@@ -90,16 +119,21 @@ class Router extends HTMLBuilder{
         exit;
     }
 
+    /**
+     * Get a route parameter by name.
+     *
+     * @param string $paramName
+     * @return mixed|null
+     */
     public function getRouteParam($paramName) {
         return $this->currentRouteParams[$paramName] ?? null;
     }
 
     /**
-     * This method is called from inside of matchRoute(),
-     * to handle a error if a route is not available.
+     * Handle a route not found error.
      *
-     * @param    string     $url URL for the route
-     * @param    string     $method the Method like "GET" or "POST 
+     * @param string $url The URL for the route
+     * @param string $method The HTTP method (e.g., "GET" or "POST")
      */
     private static function handleRouteNotFound(string $url, string $method) {
         header("HTTP/1.1 404 Not Found");
@@ -109,19 +143,19 @@ class Router extends HTMLBuilder{
     }
 
     /**
-     * Set a url to redirect to a route if route is not defined
+     * Set a URL to redirect to if the route is not defined.
      *
-     * @param    string     $url URL for the redirect
+     * @param string $url The URL for the redirect
      */
     public function setErrorRedirect(string $url){
         return $this->redirectUrl = parent::assetUrl($url);
     }
 
     /**
-     * Set a global middleware for every route that is not in the array
+     * Add a global middleware for routes not in the specified array of URLs.
      *
-     * @param    array      $urls URLs for the redirect
-     * @param    function   $gloMiddleware
+     * @param array $urls An array of URLs for the redirect
+     * @param \Closure $gloMiddleware
      */
     public function addGlobalMiddleware(array $urls, \Closure $gloMiddleware){
         $this->globalMiddleware['function'][] = $gloMiddleware;
@@ -131,28 +165,27 @@ class Router extends HTMLBuilder{
     }
 
     /**
-     * Add a middleware to be executed before a route callback.
+     * Add a middleware to be executed before or after a route callback.
      *
      * @param \Closure $middleware The middleware function to be added.
+     * @param string $position The position (before or after)
      * @return $this
      */
-    public function addMiddleware(\Closure $middleware, array $params = []) {
+    public function addMiddleware(\Closure $middleware, string $position = "before") {
         $this->middlewares["function"][] = $middleware;
         $this->middlewares["route"][] = $this->currentRoute;
-        $this->middlewares["params"][] = $params;
+        $this->middlewares["position"][] = $position;
         return $this;
     }
 
     /**
+     * Add a route to the application.
      *
-     * Add a route to the application
-     *
-     * @param    string     $method Set the Method like "GET" or "POST. Or use "*" to use every method 
-     * @param    string     $url Set URL for the route 
-     * @param    function   $target Set what to do on the route
-     * @param    int        $statusCode Set a HTTPS status code for the route
-     * @return   object     
-     *
+     * @param string $method The HTTP method (e.g., "GET" or "POST") or "*" for any method
+     * @param string $url The URL for the route
+     * @param \Closure $target The callback function for the route
+     * @param int $statusCode The HTTP status code for the route
+     * @return object
      */
     public function addRoute(string $method, string $url, \Closure $target, int $statusCode = 0) {
         $this->currentRoute = $url; // Store the current route URL
@@ -168,16 +201,13 @@ class Router extends HTMLBuilder{
     }
 
     /**
-     *
      * Add a route group to the application.
-     * 
-     * The addGroup feature allows bundling of related routes under a common URL prefix 
-     * for better structure and organization in routing.
      *
-     * @param    string     $prefix Is the common initial part of the URLs for the bundled routes, for example, "/admin".
-     * @param    function   $callback Is a function that defines the specific routes for this group and adds them to the router.
-     * @return   object     
+     * The addGroup feature allows bundling of related routes under a common URL prefix for better structure and organization in routing.
      *
+     * @param string $prefix The common initial part of the URLs for the bundled routes (e.g., "/admin").
+     * @param \Closure $callback A function that defines the specific routes for this group and adds them to the router.
+     * @return object
      */
     public function addGroup(string $prefix, \Closure $callback) {
         // Backup current middlewares
@@ -196,28 +226,27 @@ class Router extends HTMLBuilder{
     }
 
     /**
-     * Compares the requested method and URL to defined routes,
-     * processes them, and throws an exception if no match is found.
+     * Compares the requested method and URL to defined routes, processes them, and throws an exception if no match is found.
      */
     public function matchRoute() {
 
         # URL Validation
-        $url = $_SERVER['REQUEST_URI']; // get the URL from the curet page
-        if ($url != "/") {$url = rtrim($_SERVER['REQUEST_URI'], '/');} // Remove slash from the end 
+        $url = $_SERVER['REQUEST_URI']; // Get the URL from the current page
+        if ($url != "/") {$url = rtrim($_SERVER['REQUEST_URI'], '/');} // Remove slash from the end
         $url = strtok($url, '?'); // Remove query parameters from the URL
-        $url = str_replace(ltrim(rtrim(parent::assetUrl(""), "/"), "/"), "", $url); // add basename of the application folder
-        if ($url == "/index.php") { $url = "/";} // check if url is index.php if so send to the / route
-        $url = str_replace("//", "/", $url); // remove double slash from the url
+        $url = str_replace(ltrim(rtrim(parent::assetUrl(""), "/"), "/"), "", $url); // Add the basename of the application folder
+        if ($url == "/index.php") { $url = "/";} // Check if the URL is "index.php" and redirect to the root route
+        $url = str_replace("//", "/", $url); // Remove double slashes from the URL
 
-        $method = $_SERVER['REQUEST_METHOD']; // indicates the client's action (GET, POST, etc.)
+        $method = $_SERVER['REQUEST_METHOD']; // Indicates the client's HTTP request method (e.g., GET, POST, etc.)
 
         $alias = $this->findOriginalUrlForAlias($url);
         if ($alias !== null) {
             $url = $alias; // Use the original URL
         }
 
-        // Process given action on GET, POST etc.
-        if (isset($this->routes[$method])) {            
+        // Process the given action based on the HTTP request method (e.g., GET, POST, etc.)
+        if (isset($this->routes[$method])) {
             $this->processRoutes($url, $this->routes[$method]);
         }
 
@@ -226,37 +255,34 @@ class Router extends HTMLBuilder{
             $this->processRoutes($url, $this->routes['*']);
         }
 
-        // call this if route not found and redirect url is set
-        if(!isset($this->routes[$method][$url]) && !empty($this->redirectUrl)){
-            header("Location: ".$this->redirectUrl);
+        // Redirect to the specified URL if the route is not found and a redirect URL is set
+        if (!isset($this->routes[$method][$url]) && !empty($this->redirectUrl)) {
+            header("Location: " . $this->redirectUrl);
             die;
         } else if ($this->routes[$this->currentMethod][$this->currentRoute]['ajaxOnly'] == true){
-            echo json_encode(["error" => "this is not an Ajax call"]);
+            echo json_encode(["error" => "This is not an Ajax call"]);
             die;
         } else {
             $this->handleRouteNotFound($url, $method);
             die;
         }
-
     }
 
     /**
-     *
      * Loop through the routes to find a match for the given URL.
      *
-     * @param    string     $url route URL
-     * @param    array      $routes routes with the right action
-     *
+     * @param string $url The route URL
+     * @param array $routes Routes with the appropriate action
      */
     protected function processRoutes(string $url, array $routes) {
         foreach ($routes as $routeUrl => $routeData) {
 
             // Check if the current route is restricted to AJAX requests
             if ($routeData['ajaxOnly'] && !$this->isAjaxRequest()) {
-                continue; // Skip this route if not an AJAX request
+                continue; // Skip this route if it's not an AJAX request
             }
 
-            // Convert route URL to a regular expression pattern for the ':' parameter
+            // Convert route URL to a regular expression pattern for ':' parameters
             $pattern = preg_replace('/\/:([^\/]+)/', '/(?P<$1>[^/]+)', $routeUrl);
 
             // Check if the current URL matches the pattern
@@ -266,37 +292,51 @@ class Router extends HTMLBuilder{
                 if ($routeData['statusCode'] > 0) {
                     http_response_code($routeData['statusCode']);
                 }
-                
+
                 // Execute global middleware function
-                if(!empty($this->globalMiddleware) && !in_array($url, $this->globalMiddleware["url"])) {
+                if (!empty($this->globalMiddleware) && !in_array($url, $this->globalMiddleware["url"])) {
                     call_user_func($this->globalMiddleware["function"][0]);
                 }
 
-                // Execute middleware functions
-                if(!empty($this->middlewares)){
-                    $mdPosition = array_search($url, $this->middlewares["route"]);
-                    if(is_int($mdPosition)) call_user_func($this->middlewares["function"][$mdPosition], $this->middlewares["params"][$mdPosition]);
-                }
-                
+                // Execute middleware functions before the target function
+                $this->executeMiddleware('before', $url);
+
                 // Call the target function with the extracted parameter values
                 $this->currentRouteParams = $this->sanitizeStringsArray(array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY));
                 call_user_func_array($routeData['target'], $this->currentRouteParams);
 
-                //Close the application
+                // Execute middleware functions after the target function
+                $this->executeMiddleware('after', $url);
+
+                // Close the application
                 ($this->isApi == false && $routeData['ajaxOnly'] == false) ? Router::APP_CLOSE() : exit;
             }
-
         }
     }
 
     /**
-     * Executes a method in the specified controller.
+     * Execute middleware functions based on the specified position (before or after).
+     *
+     * @param string $position The position of the middleware (before or after)
+     * @param string $url The URL for which the middleware should be executed
+     */
+    private function executeMiddleware($position, $url) {
+        if (!empty($this->middlewares)) {
+            $mdPosition = array_search($url, $this->middlewares["route"]);
+            if (is_int($mdPosition) && $this->middlewares['position'][$mdPosition] === $position) {
+                call_user_func($this->middlewares["function"][$mdPosition]);
+            }
+        }
+    }
+
+    /**
+     * Execute a method in the specified controller.
      *
      * @param string $controllerName The name of the controller in which the method should be called.
      * @param string $methodName The name of the method to be called.
      * @param array $params An optional array of parameters to be passed to the method.
      */
-    public function useController(string $controllerName, string $methodName, array $params = []){
+    public function useController(string $controllerName, string $methodName, array $params = []) {
         $params = empty($params) ? $this->currentRouteParams : array_merge($params, $this->currentRouteParams);
         $controllerClassName = 'Controllers\\' . $controllerName;
         if (class_exists($controllerClassName)) {
@@ -312,11 +352,10 @@ class Router extends HTMLBuilder{
     }
 
     /**
-     * Loads and displays a PHP file from the current directory from the /sites subfolder.
+     * Loads and displays a PHP file from the current directory in the /sites subfolder.
      *
-     * @param   string  $siteName The name of the desired file (without ".php" ending).
-     * @param   array   $variables An associative array of variables to be made available in the loaded file.
-     * 
+     * @param string $siteName The name of the desired file (without ".php" ending).
+     * @param array $variables An associative array of variables to be made available in the loaded file.
      */
     public function getSite(string $siteName, array $variables = []) {
         $siteName = str_replace(".php", '', $siteName);
@@ -354,7 +393,7 @@ class Router extends HTMLBuilder{
         if (!empty($this->currentRoute)) {
             $this->aliases[$alias] = $this->currentRoute;
         }
-        
+
         return $this;
     }
 
@@ -448,4 +487,3 @@ class Router extends HTMLBuilder{
         return !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
     }
 }
-?>
