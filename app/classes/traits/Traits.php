78f57ed89555ce77b6c3f081fbc7d0476adb054d
Leon <leon18058r@gmail.com>
2023-10-12 14:06:33 +0200

    assetUrl PHPDoc added

--- app/classes/traits/Traits.php
+++ app/classes/traits/Traits.php
@@ -2,6 +2,15 @@
 namespace Classes\Traits;
 
 trait Traits{
+    /**
+     * Generate an absolute URL for an asset based on the provided path.
+     *
+     * This function appends the provided asset path to the base URL of the script,
+     * ensuring that the URL is properly formatted, including the appropriate leading slash.
+     *
+     * @param string $path The path to the asset, relative to the root of the application.
+     * @return string The absolute URL of the asset.
+     */
     public function assetUrl($path) {
         $this->baseUrl = dirname($_SERVER['SCRIPT_NAME']);
         return ($this->baseUrl == "/") ? "/" . ltrim($path, '/') : $this->baseUrl . "/" . ltrim($path, '/');
