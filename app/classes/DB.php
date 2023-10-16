59d450930d1a161e824fd7fcdaca43c0caea8e84
Leon Schmidt <leon18058r@gmail.com>
2023-10-16 19:17:38 +0200

    cleanInput inline return

--- app/classes/DB.php
+++ app/classes/DB.php
@@ -255,10 +255,8 @@ class DB {
      */
     private function cleanInput(string $input):string {
         $input = trim($input); 
-        $input = stripslashes($input); 
-        $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8'); 
-
-        return $input;
+        $input = stripslashes($input);
+        return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
     }
 
     /**
