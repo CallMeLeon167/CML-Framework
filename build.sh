cc788c728f15f6286c64791f466e661fc22ae898
Leon Schmidt <leon18058r@gmail.com>
2023-11-26 14:39:08 +0100

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-cd ./app
-echo '======== Wechsle Verzeichnis nach ' $PWD ========;
 echo '==========================    Composer     =========================='
 composer install
 composer dump-autoload
-echo '==========================    NPM     =========================='
-npm i
 echo '==========================    DONE     =========================='
