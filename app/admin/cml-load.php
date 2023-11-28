c2c2ba7bab13987650cfd830e69c3db3a310fff1
Leon <leon18058r@gmail.com>
2023-11-28 14:23:25 +0100

    custom error handler added

--- app/admin/cml-load.php
+++ app/admin/cml-load.php
@@ -44,4 +44,61 @@ require_once dirname(__DIR__, 2).'/vendor/autoload.php';
         ini_set('error_log', $errorfile);
     }
 
-?>
+    // Enable error handling
+    set_error_handler("customError");
+    
+    // Custom error function
+    function customError($errno, $errstr, $errfile, $errline) {
+        echo "
+        <div style='
+            background-color: #e74c3c;
+            border: 1px solid #c0392b;
+            border-radius: 10px;
+            color: #fff;
+            padding: 30px;
+            margin: 30px;
+            font-family: 'Roboto', sans-serif;
+            box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+            max-width: 600px;
+            margin: auto;
+            text-align: left;
+            '>
+            <h2 style='color: #fff; font-size: 28px; margin-bottom: 15px;'>Error Details</h2>
+            <p><strong>Error:</strong> [$errno] $errstr</p>
+            <p><strong>File:</strong> $errfile</p>
+            <p><strong>Line:</strong> $errline</p>
+            <hr style='border-color: #c0392b; margin: 20px 0;'>
+    
+            <pre style='
+                white-space: pre-wrap;
+                background-color: #fff;
+                color: #333;
+                padding: 15px;
+                border-radius: 10px;
+                overflow-x: auto;
+                '>";
+    
+        // Display code snippet
+        $lines = file($errfile);
+        $start = max(0, $errline - 5);
+        $end = min(count($lines), $errline + 5);
+    
+        for ($i = $start; $i < $end; $i++) {
+            echo "<span style='color: " . ($i == $errline - 1 ? '#e74c3c' : '#333') . ";'>"
+                . "Line " . ($i + 1) . ": "
+                . htmlspecialchars($lines[$i])
+                . "</span>";
+        }
+    
+        echo "</pre>
+        </div>";
+    
+        // Pass error handling to PHP's default behavior
+        return false;
+    }
+    
+    // Trigger an error for testing
+    echo $test;  // $test is not defined and will trigger an error
+    
+    ?>
+    
