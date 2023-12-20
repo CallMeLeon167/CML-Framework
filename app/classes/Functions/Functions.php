8150ac6dee633258509c1db65809e055956abe0a
Leon <leon18058r@gmail.com>
2023-12-20 08:43:48 +0100

    v2.3.4

--- app/classes/Functions/Functions.php
+++ app/classes/Functions/Functions.php
@@ -8,7 +8,7 @@ trait Functions{
      *
      * @var string
      */
-    private static string $cml_version = "2.3.3";
+    private static string $cml_version = "2.3.4";
 
     /**
      * Retrieves the current version of the framework.
