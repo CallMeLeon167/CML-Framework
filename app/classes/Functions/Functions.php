112bbf917478045f15115811e3f8b04e247725e4
Leon Schmidt <leon18058r@gmail.com>
2023-12-22 03:06:47 +0100

    Update cml_version to 2.3.5

--- app/classes/Functions/Functions.php
+++ app/classes/Functions/Functions.php
@@ -8,7 +8,7 @@ trait Functions{
      *
      * @var string
      */
-    private static string $cml_version = "2.3.4";
+    private static string $cml_version = "2.3.5";
 
     /**
      * Retrieves the current version of the framework.
