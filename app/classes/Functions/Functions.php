8dd6fd5411e65a467389e505de21dcfa0842661d
Leon Schmidt <leon18058r@gmail.com>
2024-02-19 22:32:25 +0100

    Add isAjaxRequest() method to Functions class and remove duplicate method from Router class

--- app/classes/Functions/Functions.php
+++ app/classes/Functions/Functions.php
@@ -169,6 +169,14 @@ trait Functions{
         }
     }
 
+    /**
+     * Check if the current request is an AJAX request.
+     *
+     * @return bool
+     */
+    public function isAjaxRequest(): bool {
+        return !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+    }
 
     /**
      * Returns the absolute file path to the project's root directory.
