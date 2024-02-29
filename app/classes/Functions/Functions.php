0269a66a86420a155738f1658571c8451801d0b9
Leon <leon18058r@gmail.com>
2024-02-29 22:23:43 +0100

    Update .gitignore and Functions.php

--- app/classes/Functions/Functions.php
+++ app/classes/Functions/Functions.php
@@ -8,7 +8,7 @@ trait Functions{
      *
      * @var string
      */
-    private static string $cml_version = "2.7";
+    private static string $cml_version = "2.7.1";
 
     /**
      * Retrieves the current version of the framework.
