5169b886cbce0162ed5da7065979d29e98c74445
Leon Schmidt <leon18058r@gmail.com>
2024-03-02 20:06:13 +0100

    Update CML version to 2.8

--- app/classes/Functions/Functions.php
+++ app/classes/Functions/Functions.php
@@ -8,7 +8,7 @@ trait Functions{
      *
      * @var string
      */
-    private static string $cml_version = "2.7.1";
+    private static string $cml_version = "2.8";
 
     /**
      * Retrieves the current version of the framework.
