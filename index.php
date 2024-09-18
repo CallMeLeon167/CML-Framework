7344bee014f25092dbd3c90422d2618a18c0665c
Leon Schmidt <leon18058r@gmail.com>
2024-09-18 19:28:04 +0200

    Refactor index.php and optimize global styles and scripts loading

--- index.php
+++ index.php
@@ -18,16 +18,11 @@ $app->addCDN("link", 'rel="preconnect" href="https://fonts.googleapis.com"');
 $app->addCDN("link", 'rel="preconnect" href="https://fonts.gstatic.com" crossorigin');
 $app->addCDN("link", 'href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;400;600&display=swap" rel="stylesheet"');
 
-//Global Styles
-$app->addStyle($app->compress("styles.css"));
-
-//Global Scripts
-$app->addScript($app->compress("scripts.js"));
-
-//Global HTML
-$app->addFooter();
-
 $app->addRoute('GET', '/', function () use ($app) {
+    $app->addStyle($app->compress("styles.css"));
+    $app->addScript($app->compress("scripts.js"));
+    $app->addFooter();
+
     $apiData = $app->useController("ApiController", "getRepoData", ['url' => 'https://docs.callmeleon.de/api/data']);
     $app->setTitle("Thank you! | CML - Framework");
     $app->view("home.php", $apiData);
